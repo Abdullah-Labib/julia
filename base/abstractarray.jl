4d22bb2c53ee751fcc03f830fa3f2a28ec5d5711
Jeff Bezanson <jeff.bezanson@gmail.com>
2022-06-27 15:13:14 -0400

    fix #45825, BitArray methods assuming 1-indexing of AbstractArray

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -1139,10 +1139,10 @@ function copyto!(B::AbstractVecOrMat{R}, ir_dest::AbstractRange{Int}, jr_dest::A
     return B
 end
 
-function copyto_axcheck!(dest, src)
-    @noinline checkaxs(axd, axs) = axd == axs || throw(DimensionMismatch("axes must agree, got $axd and $axs"))
+@noinline _checkaxs(axd, axs) = axd == axs || throw(DimensionMismatch("axes must agree, got $axd and $axs"))
 
-    checkaxs(axes(dest), axes(src))
+function copyto_axcheck!(dest, src)
+    _checkaxs(axes(dest), axes(src))
     copyto!(dest, src)
 end
 
